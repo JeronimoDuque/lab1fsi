1e33864fb784bbf79abbba8a171588d2f8781d06
JeronimoDuque <jeronimo.duque1@udea.edu.co>
2026-09-22 22:27:49 -0500

    primera configuracion

--- src/main/webapp/app/shared/jhipster/constants.ts
+++ src/main/webapp/app/shared/jhipster/constants.ts
@@ -1,6 +1,6 @@
-export const MESSAGE_ALERT_HEADER_NAME = 'x-lab1fsiapp-alert';
-export const MESSAGE_ERROR_HEADER_NAME = 'x-lab1fsiapp-error';
-export const MESSAGE_PARAM_HEADER_NAME = 'x-lab1fsiapp-params';
+export const MESSAGE_ALERT_HEADER_NAME = 'x-aerolineavirtualapp-alert';
+export const MESSAGE_ERROR_HEADER_NAME = 'x-aerolineavirtualapp-error';
+export const MESSAGE_PARAM_HEADER_NAME = 'x-aerolineavirtualapp-params';
 
 export const AUTHENTICATION_TOKEN_KEY = 'jhi-authenticationToken';
 
